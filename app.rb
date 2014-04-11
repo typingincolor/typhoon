a91898656c4b35df69c837e5f98ee71b911feb9a
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2014-04-11 13:49:00 +0100

    created ScriptFactory

--- app.rb
+++ app.rb
@@ -1,18 +1,10 @@
 require 'sinatra'
 require 'json'
 require 'rest_client'
-require 'mongo'
 
 require_relative 'commands/init'
 require_relative 'ScriptEngine'
-
-include Mongo
-
-db = MongoClient.new("localhost").db("script_engine")
-
-configure do
-  set :views, "#{File.dirname(__FILE__)}/views"
-end
+require_relative 'ScriptFactory'
 
 post '/script/run' do
   request.body.rewind
@@ -32,28 +24,19 @@ post '/script/factory' do
   content_type :json
   request.body.rewind
   payload = JSON.parse request.body.read
-  script = ""
 
-  if payload["action"] == "send_email"
-    script = erb :email_script, :locals => payload["data"]
-  else
-    logger.error "unknown action"
-    return 500
-  end
+  script_factory = ScriptFactory.new
 
-  # store the script
-  coll = db.collection "scripts"
-  document = JSON.parse script
-  id = coll.insert document
+  id = script_factory.build payload
 
   {:run => "http://localhost:4567/script/#{id}/run", :script => "http://localhost:4567/script/#{id}"}.to_json
 end
 
 get '/script/:id' do
-  coll = db.collection "scripts"
-  document = coll.find("_id" => BSON::ObjectId(params[:id])).to_a
   content_type :json
-  document[0].to_json
+  script_factory = ScriptFactory.new
+  script = script_factory.get params[:id]
+  script
 end
 
 post '/at' do
